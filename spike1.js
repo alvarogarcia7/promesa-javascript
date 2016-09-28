c1b7706d609b22c2ba70db6ed1deeae60188f368
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2016-09-28 22:31:25 +0100

    Defect: do not depend on existing variables on the REPL

--- spike1.js
+++ spike1.js
@@ -9,4 +9,4 @@ return result;
 };
 Promesa.resolve = (payload) => new Promessa(payload)
 
-// Usage: Promesa.resolve(hits).then(function(p) { setTimeout(()=>{console.log('a')}, 1000) }).success()
+// Usage: Promesa.resolve([1,2,3]).then(function(p) { setTimeout(()=>{console.log('a')}, 1000) }).success()
